e5e5f82d49df460187b7d0b17d125d3600ab5bf4
Relsell <relsellglobal@gmail.com>
2022-06-22 16:36:19 +0530

    updated ui

--- app/src/main/java/com/relsellglobal/firebasedatabasedemo/MainActivityWithNavBar.kt
+++ app/src/main/java/com/relsellglobal/firebasedatabasedemo/MainActivityWithNavBar.kt
@@ -26,6 +26,8 @@ class MainActivityWithNavBar : AppCompatActivity() {
 
         //setSupportActionBar(binding.appBarMainActivityWithNavBar.toolbar)
 
+        binding.appBarMainActivityWithNavBar.
+
         binding.appBarMainActivityWithNavBar.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
