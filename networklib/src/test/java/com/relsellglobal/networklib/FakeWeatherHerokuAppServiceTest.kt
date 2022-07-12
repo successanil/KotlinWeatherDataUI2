998211f0b0357dbc9d08c39c66144496d3146f64
Anil Kukreti <relsellglobal@gmail.com>
2022-07-12 14:45:22 +0530

    added test case

--- networklib/src/test/java/com/relsellglobal/networklib/FakeWeatherHerokuAppServiceTest.kt
+++ networklib/src/test/java/com/relsellglobal/networklib/FakeWeatherHerokuAppServiceTest.kt
@@ -2,14 +2,26 @@ package com.relsellglobal.networklib
 
 import com.google.common.truth.Truth
 import com.relsellglobal.networklib.fakes.FakeWeatherHerokuAppService
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
 
 class FakeWeatherHerokuAppServiceTest {
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun `should be able to fetch CityContentNetwork List` () = runTest {
         val data = FakeWeatherHerokuAppService.getWeatherDataCityList()
         Truth.assertThat(data).isNotNull()
     }
+
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `should be able to fetch CityContentNetwork List For WorkManager` () = runTest {
+        val data = FakeWeatherHerokuAppService.getWeatherDataCityListForWorkManager()
+        Truth.assertThat(data).isNotNull()
+    }
+
+
 }
